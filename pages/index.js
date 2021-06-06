7942dd3898a41a6875d61da9f8a115fbf4174e5c
Jorge Ramirez <jorge.ramirez@air-up.com>
2021-06-06 20:15:42 +0200

    Poc de nextjs con contentful
    se crea un proyecto de next js que se conecta a contentful y crea
    paginas estaticas del contenido

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function Recipes() {
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
+
+export async function getStaticProps() {
+  const clients = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+
+  const response = await clients.getEntries({
+    content_type: 'receta',
+  })
+
+  return { props: { recetas: response.items } }
+}
+
+export default function Recipes({ recetas }) {
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className='recipe-list'>
+      {recetas.map((receta) => (
+        <RecipeCard key={receta.sys.id} recipe={receta} />
+      ))}
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
     </div>
   )
 }
